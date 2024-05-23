1805137bac577a183547df25fbe3ed23380f4ea5
nusratdevo <nusratdevo@gmail.com>
2024-05-23 18:11:12 +0600

    new

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,10 +2,9 @@ terraform {
   backend "s3" {
     bucket         = "aws-eks-baket1"
     region         = "us-east-1"
-    key            = "terraform-config/EKS-TF/terraform.tfstate"
+    key            = "EKS-TF/terraform.tfstate"
     encrypt        = true
   }
-  required_version = ">=0.13.0"
   required_providers {
     aws = {
       version = "~> 5.0"
